a0496138155de6b2e1026ba37e838f939b76e59d
Iro Enezli <enezliro@gmail.com>
2018-08-26 12:19:07 +0300

    added filterTask, deleteTask

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ loadEventListeners();
 function loadEventListeners() {
 
   form.addEventListener('submit', addTask);
+  // Remove task event
+  taskList.addEventListener('click', removeTask);
+  // CLear task event
+  clearBtn.addEventListener('click', clearTasks);
+  // Filter tasks event
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e) {
@@ -40,4 +46,39 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+// Remove Task
+function removeTask(e) {
+  if(e.target.parentElement.classList.contains('delete-item')) {
+    if(confirm('Are You Sure?')) {
+      e.target.parentElement.parentElement.remove();
+  }
+ }
+}
+
+//Clear Tasks
+function clearTasks() {
+  //taskList.innerHTML = '';
+
+  // Faster
+
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach
+  (function(task) {
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
